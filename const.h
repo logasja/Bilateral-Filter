bb093e75fa51f188c30fa11133d63901634502f7
Jacob Logas <Jakeman893@ufl.edu>
2016-12-01 12:30:42 -0500

    Add output folder

--- const.h
+++ const.h
@@ -2,4 +2,4 @@
 
 const char* inputPath = "../Test Images/test-medium.jpg";
 const char* inputPathBW = "../Test Images/test-medium-bw.jpg";
-const char* outputPath = "../Test Images/test-medium.jpg";
+const char* outputPath = "../Test Images/outputs/test-medium.jpg";
